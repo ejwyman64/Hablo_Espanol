48fbba16d75a07a5e016c675a2d815ce6c475d06
Emma Wyman <ejpink4@gmail.com>
2018-09-27 19:49:30 -0400

    added a second button to the conversation

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,30 @@
-const button = document.getElementById('click-me-1');
-var helloUser = document.getElementById('hello-user');
+const button1 = document.getElementById('click-me-1');
+const button2 = document.getElementById('click-me-2');
+let helloUser = document.getElementById('hello-user');
+let yourAge = document.getElementById('user-age');
 
+let userName;
+let userAge;
 
-button.addEventListener('click', function (event) {
-    var userName = prompt('What is your name?');
-    console.log(userName);
-    if (userName !== null) {
-        helloUser.innerHTML += '¡Hola, ' + userName + '!';
-    }
-});
+function clickMyButton() {
+    button1.addEventListener('click', function (event) {
+        userName = prompt('What is your name?');
+        if (userName !== '') {
+            helloUser.innerHTML = '¡Hola, ' + userName + '!';
+        } else {
+            alert("I'm sorry, I didn't catch that. Please try again.");
+        };
+    });
+}
+
+function clickMyButton() {
+    button2.addEventListener('click', function (event) {
+        userAge = prompt('What is your name?');
+        if (userAge !== '') {
+            yourAge.innerHTML = 'Tu tiene ' + userAge + ' años.';
+        } else {
+            alert("I'm sorry, I didn't catch that. Please try again.");
+        };
+    });
+}
+clickMyButton();
